2a0f8293a0a952a0cda51e8d1944d42524784060
AmrIbrahim41 <Amrhima41@gmail.com>
2025-12-22 04:47:48 +0200

    edit message

--- Back-End/project/urls.py
+++ Back-End/project/urls.py
@@ -5,8 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')), 
-    path('api/', include('store.urls')),  
+    
+    path('api/users/', include('users.urls')),    
+    path('api/', include('store.urls')),    
+    path('', include('store.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
