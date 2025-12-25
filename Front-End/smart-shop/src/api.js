c28b3df94849e1353c600f2ba71f11e4372c54a4
AmrIbrahim41 <Amrhima41@gmail.com>
2025-12-26 01:27:39 +0200

    api fix

--- Front-End/smart-shop/src/api.js
+++ Front-End/smart-shop/src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/";
-
+const BASE_URL = "https://Amr41.pythonanywhere.com/";
 // 👇 التعديل هنا: شيلنا الهيدر الثابت عشان يسمح برفع الملفات
 const api = axios.create({
   baseURL: BASE_URL,
